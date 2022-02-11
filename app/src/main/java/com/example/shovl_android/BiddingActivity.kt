7fddc5f5667f7f35be1a1663700e9b8dcfbc9f54
Sajan Tamang <sajantmg2@gmail.com>
2022-02-11 02:09:31 -0500

    basic template. guys please complete your desings as the divided tasks

---
+++ app/src/main/java/com/example/shovl_android/BiddingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shovl_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BiddingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bidding)
+    }
+}
---
+++ app/src/main/java/com/example/shovl_android/PaymentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shovl_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
