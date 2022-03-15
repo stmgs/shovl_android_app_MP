7af846484a24686ecaae6fcbe6fa6e696f5c8d0b
Sajan Tamang <sajantmg2@gmail.com>
2022-03-15 16:27:22 -0400

    if users are previously logged in they will be directed to mainactivity otherwise to login activity

--- app/src/main/java/com/example/shovl_android/Splashscreen.kt
+++ app/src/main/java/com/example/shovl_android/Splashscreen.kt
@@ -5,21 +5,32 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shovl_android.databinding.ActivitySplashscreenBinding
+import com.example.shovl_android.utilities.PreferenceMangager
+import com.example.shovl_android.utilities.ShovlConstants
 
 
 class Splashscreen : AppCompatActivity() {
     private lateinit var binding : ActivitySplashscreenBinding
+    private lateinit var preferenceMangager: PreferenceMangager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preferenceMangager = PreferenceMangager(applicationContext)
+
         Handler().postDelayed(Runnable { //This method will be executed once the timer is over
             // Start your app main activity
-            val i = Intent(this, LoginActivity::class.java)
-            startActivity(i)
-            // close this activity
-            finish()
+            if (preferenceMangager.getBoolean(ShovlConstants.KEY_IS_SIGNED_IN)){
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+
+            } else{
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
         }, 2000)
     }
 }
