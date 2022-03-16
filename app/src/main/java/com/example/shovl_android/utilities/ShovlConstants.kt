08dcf3468d3b8f635242fef7bfc3b75135e2df56
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2022-03-16 12:38:31 -0400

    Revert "Working on Details screen"
    This reverts commit 3e5652aac7cb5e374b68e0c97f794c7b90a5f05c.

---
+++ app/src/main/java/com/example/shovl_android/utilities/ShovlConstants.kt
@@ -0,0 +1,18 @@
+package com.example.shovl_android.utilities
+
+class ShovlConstants {
+
+    companion object{
+        const val KEY_COLLECTION_USERS="users"
+        const val KEY_EMAIL="email"
+         const val KEY_PASSWORD="password"
+         const val KEY_NAME="name"
+         const val KEY_AGE="age"
+         const val KEY_ADDRESS="location"
+         const val KEY_GENDER="gender"
+         const val KEY_IS_SIGNED_IN="isSignedIn"
+         const val KEY_USER_ID="userId"
+         const val KEY_PERFERENCE_NAME="shovlPreference"
+         const val KEY_IMAGE="dp"
+    }
+}
