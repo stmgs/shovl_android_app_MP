0e15ccb5291f223b7f21b14c8ce4c50132888263
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2022-05-29 18:09:02 -0400

    PaymentGateway is ready(without merge )

---
+++ app/src/main/java/com/example/shovl_android/PaymentActivity1.java
@@ -0,0 +1,217 @@
+package com.example.shovl_android;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentActivity1 extends AppCompatActivity {
+
+    Button btnpay;
+    String SECRET_KEY="sk_test_51L4qAgKDCJiANE10hYGDVgZ6mluJl15oYjSdVSu6BM0nFW4QtCReu4YWSG73MZmsuovsblG21M22VpChGnRC7MQl003QlaG6C9",
+    PUBLISH_KEY="pk_test_51L4qAgKDCJiANE10DbHvVSKuGjWs70OE5mLK6SVqsyMpkGYDAM54CM3SEVkT9WMtaloaz54UBTiWiO54UPNJj6Et00fKAWQWqh";
+
+    PaymentSheet paymentSheet;
+
+    String customerID;
+    String EphericalKey;
+    String ClientSecret;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+
+        PaymentConfiguration.init(this, PUBLISH_KEY);
+
+        paymentSheet = new PaymentSheet(this,paymentSheetResult -> {
+
+            onPaymentResult(paymentSheetResult);
+        });
+
+        btnpay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PaymentFlow();
+            }
+        });
+
+        StringRequest stringRequest= new StringRequest(Request.Method.POST,
+                "https://api.stripe.com/v1/customers",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            customerID = object.getString("id");
+                            Toast.makeText(PaymentActivity1.this, customerID, Toast.LENGTH_SHORT).show();
+
+                            getEphericalKey(customerID);
+
+                        } catch (JSONException e) {
+
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+            // Authoraization Tool
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization","Bearer "+SECRET_KEY);
+                return header;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(PaymentActivity1.this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void onPaymentResult(PaymentSheetResult paymentSheetResult) {
+        if(paymentSheetResult instanceof PaymentSheetResult.Completed){
+            Toast.makeText(this, "Payment Success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getEphericalKey(String customerID) {
+        StringRequest stringRequest= new StringRequest(Request.Method.POST,
+                "https://api.stripe.com/v1/ephemeral_keys",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            EphericalKey = object.getString("id");
+                            Toast.makeText(PaymentActivity1.this, EphericalKey, Toast.LENGTH_SHORT).show();
+
+                            getClientSecret(customerID, EphericalKey);
+
+                        } catch (JSONException e) {
+
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+            // Authoraization Tool
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization","Bearer "+SECRET_KEY);
+                header.put("Stripe-Version","2020-08-27");
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer",customerID);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(PaymentActivity1.this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void getClientSecret(String customerID, String ephericalKey) {
+
+        StringRequest stringRequest= new StringRequest(Request.Method.POST,
+                "https://api.stripe.com/v1/payment_intents",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            ClientSecret = object.getString("client_secret");
+                            Toast.makeText(PaymentActivity1.this, ClientSecret, Toast.LENGTH_SHORT).show();
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+            // Authoraization Tool
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization","Bearer "+SECRET_KEY);
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer",customerID);
+                params.put("amount","1000"+"00");
+                params.put("currency","usd");
+                params.put("automatic_payment_methods[enabled]","true");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(PaymentActivity1.this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void PaymentFlow() {
+        paymentSheet.presentWithPaymentIntent(
+                ClientSecret,new PaymentSheet.Configuration("Shovler",
+                        new PaymentSheet.CustomerConfiguration(
+                                customerID,
+                                EphericalKey
+                        ))
+        );
+    }
+
+
+}
